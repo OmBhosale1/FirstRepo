3e489d9f1e6e0018f1f1235f38d496ba6de3f802
OmBhosale1 <bhosaleom03@gmail.com>
2025-05-10 19:26:29 -0700

    Update BuisnessLayer1.java

--- StudentAdmissionWeb/src/main/java/DynamicWebAssignments/BuisnessLayer1.java
+++ StudentAdmissionWeb/src/main/java/DynamicWebAssignments/BuisnessLayer1.java
@@ -6,5 +6,10 @@ public class BuisnessLayer1 {
 		DataAccessLayer dal = new DataPersistanceLayer1();
 		return dal.isValid(studentname, email);
 	}
+	public void insert(String studentname ,String email) {
+		DataAccessLayer dal = new DataPersistance2();
+		dal.isValid(studentname, email);
+	}
+	
 	
 }
